024c0cba414512105e5472c5b9eae69c7f4bb02a
NathanZMacedo <nathanzmacedo@gmail.com>
2023-04-06 17:35:09 -0300

    bug arrumado

--- Scripts/get.js
+++ Scripts/get.js
@@ -4,7 +4,7 @@ async function PegarDados(){
     const resultado = await fetch(url);
     const resultadoConvertido = await resultado.json();
     const array = Object.values(resultadoConvertido);
-    const arr = Object.entries(resultadoConvertido).map(([id,values])=>([id,...values]));
+    const arr = Object.entries(resultadoConvertido).map(([id,values])=>({id,...values}));
     console.log(arr)
     console.log(array)
     console.log(resultadoConvertido);
