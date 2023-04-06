acbfa646644ef8818c8f4fc005d1cc86539c8082
NathanZMacedo <nathanzmacedo@gmail.com>
2023-04-06 17:32:09 -0300

    criando o get

---
+++ Scripts/get.js
@@ -0,0 +1,12 @@
+const url ="https://crud-javascript-ff277-default-rtdb.firebaseio.com/pessoas.json"
+
+async function PegarDados(){
+    const resultado = await fetch(url);
+    const resultadoConvertido = await resultado.json();
+    const array = Object.values(resultadoConvertido);
+    const arr = Object.entries(resultadoConvertido).map(([id,values])=>([id,...values]));
+    console.log(arr)
+    console.log(array)
+    console.log(resultadoConvertido);
+}
+PegarDados();
