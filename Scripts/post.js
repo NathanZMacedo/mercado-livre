0cb6b0fae02e6fac94241055d4a7405e6fbcff41
NathanZMacedo <nathanzmacedo@gmail.com>
2023-04-06 17:03:34 -0300

    post funcionando

--- Scripts/post.js
+++ Scripts/post.js
@@ -1,3 +1,27 @@
 function sendData() {
-    console.log ("enviei")
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+
+    const data ={
+        name: name,
+        email: email,
+    }
+    // fetch(url,{argumento})
+    // metodos
+    // POST envia dados
+    // GET recebe dados
+    // DELETE deleta os dados
+    // PUT subistitue todo o objeto
+    // PATCH substitui só os argumentos recebidos
+    fetch("https://crud-javascript-ff277-default-rtdb.firebaseio.com/pessoas.json",{
+        method: "POST",
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+
+    })
+        .then(resposta => resposta.json())
+        .then(data => console.log(data))
+        .catch(erro=>console.error(erro))
 }
