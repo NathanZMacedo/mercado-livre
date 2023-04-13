787351d0990d670ea7a5cab6b962a72a9498dcb6
NathanZMacedo <nathanzmacedo@gmail.com>
2023-04-13 16:25:12 -0300

    limpando para pegar os dados e por em tela

--- Scripts/get.js
+++ Scripts/get.js
@@ -3,10 +3,10 @@ const url ="https://crud-javascript-ff277-default-rtdb.firebaseio.com/pessoas.js
 async function PegarDados(){
     const resultado = await fetch(url);
     const resultadoConvertido = await resultado.json();
-    const array = Object.values(resultadoConvertido);
+//  const array = Object.values(resultadoConvertido);
     const arr = Object.entries(resultadoConvertido).map(([id,values])=>({id,...values}));
-    console.log(arr)
-    console.log(array)
-    console.log(resultadoConvertido);
+//  console.log(arr)
+//  console.log(array)
+//  console.log(resultadoConvertido);
 }
 PegarDados();
