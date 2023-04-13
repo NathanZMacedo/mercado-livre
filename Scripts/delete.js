90228073765c80dc0b721382782b1e033f85bbf7
NathanZMacedo <nathanzmacedo@gmail.com>
2023-04-13 17:17:10 -0300

    delete ok

--- Scripts/delete.js
+++ Scripts/delete.js
@@ -1,3 +1,14 @@
+const urlDelete = "https://crud-javascript-ff277-default-rtdb.firebaseio.com/pessoas/"
+
 function deleteData(id){
-    console.log(id)
+    fetch(`${urlDelete}${id}.json`,{
+        method: 'DELETE',
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res=>{
+        PegarDados();
+    })
+    .catch(err=>console.error("Erro ak deletar dado", err))
 }
