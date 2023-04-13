89195c9e5b823505c04761a2b968023d7f5e4d2a
NathanZMacedo <nathanzmacedo@gmail.com>
2023-04-13 17:28:14 -0300

    criando função

---
+++ Scripts/update.js
@@ -0,0 +1,9 @@
+// DIFERENÇA DE PATCH VS PUT
+// PATCH MODIFICA APENAS OS ATRIBUTOS ENVIADOS
+// PUT MODIFICCA O OBJETO INTEIRO
+
+var editMode = document.getElementById("submit").innerHTML = "Edit Aqui!"
+
+function EnviandoDadosAtuais(id,nameItem,emailItem){
+    
+}
